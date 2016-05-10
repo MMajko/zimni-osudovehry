34df4dac6a9f05b4ebf1ecbcd17282b730e5ccd6
Marián Hlaváč <hlavac95@gmail.com>
2016-05-10 15:37:33 +0200

    Use favicon

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var app = express();
 app.set('views', path.join(__themedir, 'views'));
 app.set('view engine', 'hbs');
 
-//app.use(favicon(path.join(__themedir, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__themedir, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
